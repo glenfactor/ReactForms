db50305c4f332af9e3c31626b6aad770cbd71bc0
Glen Factor <glenfactor@gmail.com>
2023-07-24 19:28:11 -0400

    adding updates

--- src/SignUpForm.jsx
+++ src/SignUpForm.jsx
@@ -1,7 +1,21 @@
+import { useState } from 'react'
+const [username, setUsername] = useState('');
+const [password, setPassword] = useState('');
+const [error, setError] = useState(null);
+
+
 export default function SignUpForm() {
     return (
         <div>
             <h2>Sign Up</h2>
+           <form>
+            <label>Username:</label></>
+               <input value={username} onChange={e => setUsername(e.target.value)}/>
+               <label>Password:</label></>
+               <input value={password} onChange={e => setPassword(e.target.value)} />
+               <button>Submit</button>
+           </form>
+
         </div>
     )
 
