d518fa2718bc1ab3a4369b01562f48be7956794c
Glen Factor <glenfactor@gmail.com>
2023-07-25 17:16:34 -0400

    updated App.jsx

--- src/SignUpForm.jsx
+++ src/SignUpForm.jsx
@@ -1,22 +1,30 @@
-import { useState } from 'react'
-const [username, setUsername] = useState('');
-const [password, setPassword] = useState('');
+import React, { useState } from 'react';
+
+export default function SignUpForm() {
+const [username, setUsername] = useState("");
+const [password, setPassword] = useState("");
 const [error, setError] = useState(null);
 
 
-export default function SignUpForm() {
+    async function handleSubmit(event) {
+        event.preventDefault();
+        console.log("Hello 👋");
+    }
+
     return (
         <div>
             <h2>Sign Up</h2>
-           <form>
-            <label>Username:</label></>
-               <input value={username} onChange={e => setUsername(e.target.value)}/>
-               <label>Password:</label></>
-               <input value={password} onChange={e => setPassword(e.target.value)} />
-               <button>Submit</button>
-           </form>
-
+            <form onSubmit={handleSubmit}>
+                <label>Username:
+                    <input value={username} onChange={e => setUsername(e.target.value)}/>
+                </label>
+                <label>Password:
+                    <input value={password} onChange={e => setPassword(e.target.value)} />
+                </label>
+                <button type='submit'>Submit</button>
+            </form>
         </div>
+
     )
 
 }
