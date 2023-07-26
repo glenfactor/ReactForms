350bc5be89bc473719db74a6dad6503dc8023a07
Glen Factor <glenfactor@gmail.com>
2023-07-26 19:21:57 -0400

    Updated SignUpForm.jsx

--- src/SignUpForm.jsx
+++ src/SignUpForm.jsx
@@ -8,18 +8,43 @@ const [error, setError] = useState(null);
 
     async function handleSubmit(event) {
         event.preventDefault();
-        console.log("Hello 👋");
-    }
+        const form = event.target;
+        const data = new FormData(form);
+        console.log(data);
 
+        try {
+            const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {
+              method: "POST",
+              headers: {
+                'Content-Type': 'application/json' // Set the Content-Type header to JSON
+              },
+              body: JSON.stringify({ username: username, password: password }),
+            });
+            const result = await response.json();
+            console.log(result);
+          } catch (error) {
+            console.log(error);
+          }
+          setUsername("");
+          setPassword("");
+        }
+        console.log(username, password);
     return (
         <div>
-            <h2>Sign Up</h2>
+            <h2>Sign Up</h2> 
+            {error && <p>{error}</p>}
             <form onSubmit={handleSubmit}>
-                <label>Username:
-                    <input value={username} onChange={e => setUsername(e.target.value)}/>
+                <label>Username: {" "}
+                    <input 
+                    type="text"
+                    value={username} 
+                    onChange={e => setUsername(e.target.value)}/>
                 </label>
-                <label>Password:
-                    <input value={password} onChange={e => setPassword(e.target.value)} />
+                <label>Password:{""}
+                    <input 
+                    type="password"
+                    value={password} 
+                    onChange={e => setPassword(e.target.value)} />
                 </label>
                 <button type='submit'>Submit</button>
             </form>
