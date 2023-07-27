b48bcb6c3b58d7d51445723c6dfef08ed05dd5e5
Glen Factor <glenfactor@gmail.com>
2023-07-27 17:02:08 -0400

    updated files for submission

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-
-import Authenticate from './Authenticate'
-import SignUpForm from './SignUpForm'
-import './App.css'
+import { useState } from 'react';
+import SignUpForm from './SignUpForm';
+import Authenticate from './Authenticate';
+import './App.css';
 
 function App() {
+  const [token, setToken] = useState("");
 
   return (
     <>
-    <Authenticate />
-    <SignUpForm />
-    
+      <SignUpForm token={token} setToken={setToken} />
+      <Authenticate token={token} setToken={setToken} />
       <p className="read-the-docs">
         Project Collaboration: Kalvin Zheng & Glen Factor
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
